e5af571ea2f14be4bb3eae94967cb16c85cff0b8
Joe Sanders <kernel528@gmail.com>
2024-11-02 14:46:10 -0500

    Added /dishes route with listDishes GET handler.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,31 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+const createDishes = (req, res) => {
+    const { data } = req.body;
+
+    // Check for `data` object and 'href' within it
+    if (!data || !data.href) {
+        return res.status(400).json({ error: "Dishes URL (href) is required" });
+    }
+
+    // Create new dishId
+    const newDish = { href: data.href, id: dishes.length + 1 };
+    dishes.push(newDish);
+
+    dishes.id = nextId();
+    dishes.push(dishes);
+    res.json({ data: dishes });
+};
+
+// List dishes
+const listDishes = (req, res) => {
+  res.json({ data: dishes });
+};
+
+module.exports = {
+  createDishes,
+  // read,
+  // update,
+  listDishes,
+};
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+const dishesController = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const notFound = require("../errors/notFound");
+const errorHandler = require("../errors/errorHandler");
+
+router
+    .route("/")
+    .get(dishesController.listDishes)
+    .all(methodNotAllowed);
 
 module.exports = router;
